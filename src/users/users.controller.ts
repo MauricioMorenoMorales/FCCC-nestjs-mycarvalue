6420eef181a87c4200f3d9ea7d575df75c837fd9
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-03-07 09:41:37 -0600

    Eliminando contraseñas con excludes en la entity

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,10 +3,13 @@ import {
 	Controller,
 	Delete,
 	Get,
+	NotFoundException,
 	Param,
 	Patch,
 	Post,
 	Query,
+	UseInterceptors,
+	ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
@@ -20,11 +23,15 @@ export class UsersController {
 		this.usersService.create(body.email, body.password);
 	}
 
+	@UseInterceptors(ClassSerializerInterceptor) // requiere que el entity posea un decorador exclude
 	@Get('/:id')
-	public findUser(@Param('id') id: string) {
-		return this.usersService.findOne(parseInt(id));
+	public async findUser(@Param('id') id: string) {
+		const user = await this.usersService.findOne(parseInt(id));
+		if (!user) throw new NotFoundException('The user was not found');
+		return user;
 	}
 
+	@UseInterceptors(ClassSerializerInterceptor)
 	@Get()
 	public findAllUsers(@Query('email') email: string) {
 		return this.usersService.find(email);
