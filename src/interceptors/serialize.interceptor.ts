de4381d76def2dff575f6c010f6194013de51f79
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-03-08 12:38:17 -0600

    Signin and signup created

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,10 +7,9 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs';
 import { plainToClass } from 'class-transformer';
-import { nextTick } from 'process';
 
 interface ClassConstructor {
-	new (...args: any[]): {};
+	new (...args: Array<any>): {};
 }
 
 export const Serialize = (dto: ClassConstructor) =>
@@ -24,11 +23,11 @@ export class SerializeInterceptor implements NestInterceptor {
 	): Observable<any> {
 		// Run something before a request is handled by the request handler
 		return handler.handle().pipe(
-			map((data: any) => {
-				//Run something before the response is sent out
-				//Filtra la data comparandola con un dto
-				return plainToClass(this.dto, data, { excludeExtraneousValues: true });
-			}),
+			//Run something before the response is sent out
+			//Filtra la data comparandola con un dto
+			map((data: any) =>
+				plainToClass(this.dto, data, { excludeExtraneousValues: true }),
+			),
 		);
 	}
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -16,14 +16,23 @@ import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-	constructor(private usersService: UsersService) {}
+	constructor(
+		private usersService: UsersService,
+		private authService: AuthService,
+	) {}
 	@Post('signup')
 	public createUser(@Body() body: CreateUserDto) {
-		this.usersService.create(body.email, body.password);
+		return this.authService.signup(body.email, body.password);
+	}
+
+	@Post('signin')
+	public signin(@Body() body: CreateUserDto) {
+		return this.authService.signin(body.email, body.password);
 	}
 
 	//? @UseInterceptors(ClassSerializerInterceptor) // requiere que el entity posea un decorador exclude
